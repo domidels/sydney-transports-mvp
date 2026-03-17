574707715e4205728734f4bc8f7381d9d2a6f569
domidels <domidels@gmail.com>
2026-03-17 17:10:52 +1100

    Fixing map height to fill screen on mobile.

--- services/web_frontend/app.js
+++ services/web_frontend/app.js
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "373":  "#9333ea", // purple
     "379":  "#0891b2", // cyan
     "380":  "#65a30d", // lime
-    "381":  "#ca8a04", // yellow
+    "381":  "#06f822b9", // light green
     "390X": "#ea580c", // orange
     "726e": "#6366f1", // indigo
   };
